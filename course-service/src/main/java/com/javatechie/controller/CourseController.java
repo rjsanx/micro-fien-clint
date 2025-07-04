53892c29caead569e0cb6f4a1fa5a988312c2a46
rjsanx <supratiksantra67@gmail.com>
2025-07-04 13:18:17 +0530

    second commit

--- course-service/src/main/java/com/javatechie/controller/CourseController.java
+++ course-service/src/main/java/com/javatechie/controller/CourseController.java
@@ -11,7 +11,7 @@ import java.util.List;
 
 @RestController
 @RequestMapping("/api/courses")
-public class CourseController {
+public class CourseController   {
 
     @Autowired
     private CourseService courseService;
